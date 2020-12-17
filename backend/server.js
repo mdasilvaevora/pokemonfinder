d212d2ff56116237d1a7252dc6894b89a3afbb9d
mdasilvaevora <mdasilvaevora@gmail.com>
2020-12-17 14:40:12 -0300

    Fixing server on heroku

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,9 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 // 2) Routes
 app.use(routes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'../client/build/index.html'));
+});
 
 
 const port = process.env.PORT || 5000; 
